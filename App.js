aabe836512aa426fad84afeed291d8b4fc4dbb1a
Rajat Kumar <rajatrana.rk@gmail.com>
2023-04-23 23:56:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import CurrentWeather from "./src/screens/CurrentWeather"
+import UpcommingWeather from "./src/screens/UpcommingWeather"
+import City from "./src/screens/City"
+import { NavigationContainer } from "@react-navigation/native"
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+const Tab = createBottomTabNavigator()
+const App = ()=>{
+  return(
+  <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name={'Current'} component={CurrentWeather}/>
+      <Tab.Screen name={'Upcomming'} component={UpcommingWeather}/>
+      <Tab.Screen name={'City'} component={City}/>
+   </Tab.Navigator>
+   </NavigationContainer>
+  )
+}
+
+export default App
